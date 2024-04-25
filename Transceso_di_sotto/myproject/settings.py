74e5b40086cf4f6d223132c99e2c1a78c98cf1d2
marco arimattei <marco.arimattei@gmail.com>
2024-04-26 01:01:50 +0200

    functioning docker compose file

--- Transceso_di_sotto/myproject/settings.py
+++ Transceso_di_sotto/myproject/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-(5m+tq!7$q!q6r+x=sqn5%_1_oap!uhl@wx$=3+%-uvjio%0pa
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -76,7 +74,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR /  'db' / 'db.sqlite3',
     }
 }
 
