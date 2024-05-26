58ee3cc1294b238efd6a7487ac19276fe8d09fbf
Flavio Grossi <dafla107@gmail.com>
2024-05-26 12:35:17 +0200

    finally fixed reload problem

--- django-app/myapp/urls.py
+++ django-app/myapp/urls.py
@@ -1,6 +1,12 @@
+# urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
+    path('start/', views.start, name='start'),
+    path('game/', views.game, name='game'),
+    path('chat/', views.chat, name='chat'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('friends/', views.friends, name='friends'),
 ]
--- django-app/myapp/views.py
+++ django-app/myapp/views.py
@@ -1,19 +1,31 @@
+# views.py
 from django.shortcuts import render
+from django.http import JsonResponse
 
 def home(request):
     return render(request, 'home.html')
 
+def start(request):
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        return render(request, 'start.html')
+    return render(request, 'home.html')
+
 def game(request):
-    return render(request, 'game.html')
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        return render(request, 'game.html')
+    return render(request, 'home.html')
 
 def chat(request):
-    return render(request, 'chat.html')
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        return render(request, 'chat.html')
+    return render(request, 'home.html')
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
-    
-def friends(request):
-    return render(request, 'friends.html')
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        return render(request, 'dashboard.html')
+    return render(request, 'home.html')
 
-def start(request):
-    return render(request, 'start.html')
+def friends(request):
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        return render(request, 'friends.html')
+    return render(request, 'home.html')
