8a2adaa7fc4bf07e36c13853ded40ad4aced954c
cbuszyns <cbuszyns@LAPTOP-B2LAH0N3.localdomain>
2024-06-07 16:38:23 +0200

    added migrations, working on how to use database fields in django

--- django-app/myapp/models.py
+++ django-app/myapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class user(models.Model):
+	name = models.CharField(max_length=8)
+	email = models.EmailField()
+	pongWins = models.IntegerField()
+	pongLoses = models.IntegerField()
+
+class game(models.Model):
+	playerRigth = models.CharField()
+	scoreRight = models.SmallIntegerField()
+	playerLeft = user()
+	scoreLeft = models.SmallIntegerField()
+	round = models.SmallIntegerField()
+
---
+++ django-app/script.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python3 manage.py runserver --noreload 0.0.0.0:8000 && python3 manage.py migrate;
