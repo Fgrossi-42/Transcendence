93edcdf3e92f66f175815acbce3ebd2150194192
cbuszyns <cbuszyns@LAPTOP-B2LAH0N3.localdomain>
2024-06-14 16:33:44 +0200

    fixed minor problems with migrations

--- django-app/myapp/management/commands/wait_for_db.py
+++ django-app/myapp/management/commands/wait_for_db.py
@@ -1,26 +1,50 @@
-import django
-import time
-import os
+# import django
+# import time
+# import os
+# from django.core.management.base import BaseCommand
+# from django.db import connections
+# from django.db.utils import OperationalError
+
+# class Command(BaseCommand):
+#     """Django command to pause execution until database is available"""
+# DATABASE_READY = False
+
+# def handle():
+#   while not DATABASE_READY:
+#     try:
+#       django.db.connections['default'].ensure_connection()
+#     except Exception:
+#       print('Waiting for db...')
+#       time.sleep(1)
+#     else:
+#       DATABASE_READY = True
+
+#   if not DATABASE_READY:
+#     print('Applying migrations...')
+#     os.system('python manage.py migrate')
+
+
 from django.core.management.base import BaseCommand
 from django.db import connections
 from django.db.utils import OperationalError
+import time
+import django
+
 
 class Command(BaseCommand):
-    """Django command to pause execution until database is available"""
-
-# In settings.py
-DATABASE_READY = False
-
-# At startup
-while not DATABASE_READY:
-  try:
-    django.db.connections['default'].ensure_connection()
-  except Exception:
-    time.sleep(1)
-  else:
-    DATABASE_READY = True
-
-# Run migrations now
-if not DATABASE_READY:
-  print('Applying migrations...')
-  os.system('python manage.py migrate')
+    """ Django command to pause execution until database is available"""
+    def handle(self, *args, **kwargs):
+        self.stdout.write('waiting for db ...')
+        db_conn = False
+        while not db_conn:
+            try:
+                # get the database with keyword 'default' from settings.py
+                # db_conn = connections['default']
+                self.check(databases=['default'])
+                db_conn = True
+                # django.db.connections['default'].ensure_connection()
+                # prints success messge in green
+                self.stdout.write(self.style.SUCCESS('db available'))
+            except Exception:
+                self.stdout.write("Database unavailable, waiting 1 second ...")
+                time.sleep(1)
--- django-app/scripts/script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-python3 manage.py runserver --noreload 0.0.0.0:8000 
-#&& python3 manage.py migrate;
